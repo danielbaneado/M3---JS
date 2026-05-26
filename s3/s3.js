f0c3d3dd7c887ee5baf3fbffda2768f63c7fad70
Ricardo031 <rjtb31@gmail.com>
2026-05-26 18:27:01 -0500

    task 1-2

---
+++ s3/s3.js
@@ -0,0 +1,29 @@
+const form= document.getElementById("note-form")
+let message= document.querySelector("h2")
+const userInput= document.getElementById("note-input")
+const addBtn= document.getElementById("add-btn")
+const noteList= document.getElementById("note-list")
+addBtn.addEventListener("click", (e) => {
+    e.preventDefault()
+    if (userInput.value.trim() !== ""){
+        const newNote= document.createElement("li")
+        newNote.innerHTML+= `<div class="note-container"> 
+        <p>${userInput.value}</p><button class="del-btn" type="button">Delete</button>
+        </div>`
+        noteList.appendChild(newNote)
+        message.textContent= `Note sucessfully added.`
+        message.classList.remove("error-deleted")
+        message.classList.add("added")
+        userInput.value= ""
+        form.reset()
+    }
+    else{
+        message.textContent= "Invalid input!"
+        message.classList.remove("added")
+        message.classList.add("error-deleted")
+    }
+    const delBtn= document.querySelector(".del-btn")
+    delBtn.addEventListener("click", () => {
+        console.log("sirvo")
+    })
+})
